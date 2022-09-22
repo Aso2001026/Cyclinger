0a4993b74c8a976ac9f08bde5bbbaa03703ac41e
Aso2001026 <83051005+Aso2001026@users.noreply.github.com>
2022-09-22 13:15:29 +0900

    Update db.php

--- Cyclinger/DB/db.php
+++ Cyclinger/DB/db.php
@@ -3,9 +3,9 @@ function getDB()
 {
     $pdo = new PDO(
         'mysql:host=mysql203.phy.lolipop.lan;
-dbname=LAA1290630-cyclinger;charaset=utf8',
-        'LAA1290630',
-        'PassCyclinger'
+dbname=dbname',
+        'dbname',
+        'Password'
     );
     return $pdo;
 }
